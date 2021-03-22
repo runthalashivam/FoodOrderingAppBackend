bbf33b0763d785a7d6c4f6402588ff44d92d62ba
karene85 <karenemody@gmail.com>
2021-03-22 18:44:03 +0530

    Implemented Delete Address endpoint

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/dao/AddressDao.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/dao/AddressDao.java
@@ -52,4 +52,37 @@ public class AddressDao {
             return null;
         }
     }
+
+    //Method to get customer address entity for a particular address from the database
+    public CustomerEntity getCustomerByAddress(final AddressEntity address) {
+        try {
+            CustomerAddressEntity customerAddressEntity = entityManager.createNamedQuery("customerAddressByAddressId", CustomerAddressEntity.class)
+                    .setParameter("address", address)
+                    .getSingleResult();
+            return customerAddressEntity.getCustomer();
+        } catch (NoResultException nre) {
+            return null;
+        }
+    }
+
+    //Method to get address entity by address UUID from the database
+    public AddressEntity getAddressByUuid(final String addressUuid) {
+        try {
+            return entityManager.createNamedQuery("addressByUuid", AddressEntity.class)
+                    .setParameter("addressUuid", addressUuid)
+                    .getSingleResult();
+        } catch (NoResultException nre) {
+            return null;
+        }
+    }
+
+    //Method to delete an address entity from the database
+    public boolean deleteAddress(final AddressEntity addressEntity) {
+        try {
+            entityManager.remove(addressEntity);
+        } catch(IllegalArgumentException iae) {
+            return false;
+        }
+        return true;
+    }
 }
