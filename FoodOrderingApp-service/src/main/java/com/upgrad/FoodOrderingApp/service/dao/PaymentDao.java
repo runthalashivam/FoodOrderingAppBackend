57d2b01eccd7a150cde8b58ebca5e5ebca1b0445
Ushasidas <73744453+Ushasidas@users.noreply.github.com>
2021-03-28 19:38:44 +0530

    Create PaymentDao.java

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/dao/PaymentDao.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/dao/PaymentDao.java
@@ -1,11 +1,14 @@
 package com.upgrad.FoodOrderingApp.service.dao;
 
+
 import com.upgrad.FoodOrderingApp.service.entity.PaymentEntity;
+import com.upgrad.FoodOrderingApp.service.entity.StateEntity;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
 @Repository
 public class PaymentDao {
@@ -13,6 +16,9 @@ public class PaymentDao {
     @PersistenceContext
     private EntityManager entityManager;
 
+    public List<PaymentEntity> getPaymentMethods(){
+        try {
+            return this.entityManager.createNamedQuery("allPaymentMethods", PaymentEntity.class).getResultList();
     public PaymentEntity getPaymentByUuid(final String paymentUuid) {
         try {
             return entityManager.createNamedQuery("paymentByUuid", PaymentEntity.class)
