de55171bf8f746aa048f07658bcee65347b4e7de
Ushasidas <73744453+Ushasidas@users.noreply.github.com>
2021-03-28 14:17:00 +0530

    Update SwaggerConfiguration.java

--- FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/config/SwaggerConfiguration.java
+++ FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/config/SwaggerConfiguration.java
@@ -1,10 +1,13 @@
 package com.upgrad.FoodOrderingApp.api.config;
 
 
+import io.swagger.annotations.Api;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -16,8 +19,14 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfiguration {
 
+
+    ApiInfo apiInfo = new ApiInfoBuilder().title("Foodies Junction : IIIT-B Capstone Project (Back-end)").description("API end-points for Foodies Junction Application").
+            version("1.0.1-SNAPSHOT").build();
+
     @Bean
     public Docket swagger() {
-        return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.basePackage("com.upgrad.FoodOrderingApp.api.controller")).paths(PathSelectors.any()).build();
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo).select()
+                .apis(RequestHandlerSelectors.basePackage("com.upgrad.FoodOrderingApp.api.controller")).
+                paths(PathSelectors.any()).build();
     }
 }
