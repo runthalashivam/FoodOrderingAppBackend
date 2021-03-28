891a15c3586944593a3d756dd9b95447a515f5b3
Ushasidas <73744453+Ushasidas@users.noreply.github.com>
2021-03-28 19:40:22 +0530

    Create PaymentEntity.java

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/entity/PaymentEntity.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/entity/PaymentEntity.java
@@ -1,5 +1,47 @@
 package com.upgrad.FoodOrderingApp.service.entity;
 
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
+@Entity
+@Table(
+        name = "payment"
+)
+@NamedQueries({
+        @NamedQuery(name = "allPaymentMethods", query = "select p from PaymentEntity p ")
+})
+public class PaymentEntity implements Serializable {
+
+    @Id
+    @Column(
+            name = "ID"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.IDENTITY
+    )
+    private long id;
+    //uuid must be UNIQUE & NOTNULL
+    @Column(
+            name = "UUID"
+    )
+    @Size(
+            max = 200
+    )
+    private String uuid;
+
+    @Column(
+            name = "PAYMENT_NAME"
+    )
+    //paymentName can be NULL
+    private String paymentName;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 
